ef14fa4bc5511094747690c03a4554b4f124bd8f
ChrSchu <18742601+ChrSchu90@users.noreply.github.com>
2024-03-16 22:58:11 +0100

    Add WiFiManager for WiFi setup witout hardcoded SSID and credentials

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,53 @@
 #include <Arduino.h>
 #include <Preferences.h>
+#include <WiFiManager.h>
+#include <WiFi.h>
 #include "ThermistorCalc.h"
 
+WiFiManager wfm;
 ThermistorCalc *thermistor;
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  thermistor = new ThermistorCalc(-40, 167820, 25, 6523, 120, 302);
+void test(arduino_event_t *event)
+{
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("R=7701"); 
-  double c = thermistor->celsiusFromResistance(7701);
-  Serial.println((String) "c=" + c);
-  double r = thermistor->resistanceFromCelsius(21);
-  Serial.println((String) "r=" + r);
-  delay(2000);
+/// @brief Setup for WiFiManager as none blocking implementation
+void setupWifiManager()
+{
+	// explicitly set mode, esp defaults to STA+AP
+	WiFi.mode(WIFI_STA);
+
+	// reset settings - wipe credentials for testing
+	// wm.resetSettings();
+
+	wfm.setConfigPortalBlocking(false);
+	wfm.setConfigPortalTimeout(600);
+
+	// automatically connect using saved credentials if they exist
+	// If connection fails it starts an access point with the specified name
+	if (wfm.autoConnect(wfm.getDefaultAPName().c_str(), "123456789"))
+	{
+		Serial.println("connected...yeey :)");
+	}
+	else
+	{
+		Serial.println("Configportal running");
+	}
+}
+
+/// @brief Put your setup code here, to run once:
+void setup()
+{
+	Serial.begin(115200);
+	setupWifiManager();
+	thermistor = new ThermistorCalc(-40, 167820, 25, 6523, 120, 302);
+}
+
+/// @brief Put your main code here, to run repeatedly:
+void loop()
+{
+	wfm.process();
+	// double c = thermistor->celsiusFromResistance(7701);
+	// double r = thermistor->resistanceFromCelsius(21);
+	delay(2000);
 }
